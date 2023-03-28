852edbd85aa9ed16bf16bb59d59a87e3b58affc4
advakhov97@mail.ru <advakhov97@mail.ru>
2023-03-28 13:36:15 +0300

    Добавлены папки с файлами в слой BL.

--- Project_WEB-master/eUseControl.BusinessLogic/Core/UserApi.cs
+++ Project_WEB-master/eUseControl.BusinessLogic/Core/UserApi.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace eUseControl.BusinessLogic
+namespace eUseControl.BusinessLogic.Core
 {
-    public class Class1
+    public class UserApi
     {
     }
 }
---
+++ Project_WEB-master/eUseControl.BusinessLogic/SessionBL.cs
@@ -0,0 +1,16 @@
+﻿using eUseControl.BusinessLogic.Core;
+using eUseControl.BusinessLogic.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eUseControl.BusinessLogic
+{
+    public class SessionBL : UserApi, ISession
+    {
+        
+    }
+}
